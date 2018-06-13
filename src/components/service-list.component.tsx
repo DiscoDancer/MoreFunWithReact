f1f6cb371ab1b6d0995ddac4c44dc667e5e8e0e6
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-13 20:43:46 +0300

    - dev progress

--- src/components/service-list.component.tsx
+++ src/components/service-list.component.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
 import ServiceIcon from "./service-icon.component";
+import ServiceListModel from "../models/service-list.model";
+import ApiService from "../services/api.service";
 
 interface ServiceListState {
-
+    model: ServiceListModel
 }
 
 interface ServiceListProps {
@@ -11,9 +13,30 @@ interface ServiceListProps {
 }
 
 export default class ServiceList extends React.Component<ServiceListProps, ServiceListState> {
+
+    constructor(props: ServiceListProps) {
+        super(props);
+
+        this.state = {
+            model: new ServiceListModel([]),
+        };
+    }
+
+    componentDidMount() {
+        const self = this;
+        ApiService.propertyServices.then((model) => {
+            self.setState({
+                model
+            });
+        }).catch((err) => {
+            console.log("error " + err);
+        });
+    }
+
     render() {
         return (
             <div>
+                <h2>{this.state.model.serviceIcons.length}</h2>
                 <ServiceIcon/>
                 <ServiceIcon/>
                 <ServiceIcon/>
