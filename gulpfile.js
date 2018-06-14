c6899a362acd1948e16d66fc81befb293a8e2fc8
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-14 12:35:36 +0300

    - run jest with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require("gulp");
 const webpack = require("webpack");
 const webpackConfig = require("./webpack.config.js");
+const jest = require('gulp-jest').default;
+const glob = require("glob");
+
+
 
 gulp.task("react-bundle", function () {
     webpack(webpackConfig, function (err, stats) {
@@ -8,4 +12,19 @@ gulp.task("react-bundle", function () {
             console.log(err);
         }
     });
+});
+
+gulp.task('jest', function () {
+    const tests = glob.sync("src/**/__tests__/");
+
+    return gulp.src(tests).pipe(jest({
+        "moduleFileExtensions": [
+            "ts",
+            "tsx",
+            "js"
+        ],
+        "transform": {
+            "\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+        }
+    }));
 });
