aad6844ea78167d6987c90248b81f84d977786b5
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-15 10:12:10 +0300

    - updated test data and styles

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -10,18 +10,18 @@ export default class ApiService implements DataService {
 
     getServiceListModel(propertyId: string): Promise<ServiceListModel> {
         return Promise.resolve(new ServiceListModel([
-            new ServiceIconModel("http://elo.dev.localhost/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_k.ashx?h=100&amp;w=100&amp;la=fi-FI&amp;hash=AD46B11FB0D795D9705443A66BDCFE22EF86E694",
-                "http://elo.dev.localhost/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_h.ashx?h=100&w=101&la=fi-FI&hash=025C1206A4F4B594BF55BF72F2F7D317DE19F715",
+            new ServiceIconModel("https://toimitilat.elo.fi/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_k.ashx?h=75&amp;w=75&amp;la=fi-FI&amp;hash=CDBA18119596D6C82E72B3CC99A740C86724C9CE",
+                "https://toimitilat.elo.fi/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_h.ashx?h=75&amp;w=75&amp;la=fi-FI&amp;hash=EEB4F350F70835B33356C8DBFE8F3AEC2298FB3A",
                 "Keskustan palvelut",
                 "Keskeisen sijainnin ansiosta kaikki keskustan palvelut helposti saavutettavissa."),
-            new ServiceIconModel("http://elo.dev.localhost/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_k.ashx?h=100&amp;w=100&amp;la=fi-FI&amp;hash=AD46B11FB0D795D9705443A66BDCFE22EF86E694",
-                "http://elo.dev.localhost/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_h.ashx?h=100&w=101&la=fi-FI&hash=025C1206A4F4B594BF55BF72F2F7D317DE19F715",
-                "Keskustan palvelut2",
-                "Keskeisen sijainnin ansiosta kaikki keskustan palvelut helposti saavutettavissa.2"),
-            new ServiceIconModel("http://elo.dev.localhost/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_k.ashx?h=100&amp;w=100&amp;la=fi-FI&amp;hash=AD46B11FB0D795D9705443A66BDCFE22EF86E694",
-                "http://elo.dev.localhost/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ke_h.ashx?h=100&w=101&la=fi-FI&hash=025C1206A4F4B594BF55BF72F2F7D317DE19F715",
-                "Keskustan palvelut3",
-                "Keskeisen sijainnin ansiosta kaikki keskustan palvelut helposti saavutettavissa.3"),
+            new ServiceIconModel("https://toimitilat.elo.fi/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ru_k.ashx?h=75&amp;w=75&amp;la=fi-FI&amp;hash=A02376C544198D14DA1184A844C89BC6BE4784B5",
+                "https://toimitilat.elo.fi/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/ru_h.ashx?h=75&amp;w=75&amp;la=fi-FI&amp;hash=864B9D73A68E47602E33316B1C8D45348133BC3B",
+                "Lounas",
+                "Kiinteistössä palvelevien ravintoloiden lisäksi keskustan kattava lounasravintolatarjonta helposti saavutettavissa kiinteistön loistavan sijainnin ansiosta."),
+            new ServiceIconModel("https://toimitilat.elo.fi/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/py_k.ashx?h=75&amp;w=75&amp;la=fi-FI&amp;hash=6AD90BEEED2F3589026C47358755359864821433",
+                "https://toimitilat.elo.fi/-/media/toimitilat_elo_fi-kuvat/palveluikonit/64px/py_h.ashx?h=75&amp;w=75&amp;la=fi-FI&amp;hash=42286B5103DBDE784A111B6B6148237722F2F238",
+                "Pysäköinti",
+                "Kellarikerroksessa on omia pysäköintipaikkoja ja lähietäisyydellä useita pysäköintihalleja."),
         ]));
     }
 }
